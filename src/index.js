a17cbbf26ea2a7d069d10f24cae6e08b76c0942b
mihon.testosterone <mihon.testosterone@gmail.com>
2021-07-14 11:28:56 +0300

    1
    1

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import { uuid } from 'uuidv4';
-import './styles.css';
+// import { uuid } from 'uuidv4';
+// import './styles.css';
 
-import dog from './images/dog.jpeg';
+// import dog from './images/dog.jpeg';
 
-console.log(uuid());
+// console.log(uuid());
 
-// ======== functions ========
-const sayHello = () => {
-  console.log('Hello');
-};
+// // ======== functions ========
+// const sayHello = () => {
+//   console.log('Hello');
+// };
 
-const sum = (a, b) => {
-  console.log(a + b);
-};
+// const sum = (a, b) => {
+//   console.log(a + b);
+// };
 
-// ======== run ========
-sayHello();
-sum(20, 9);
+// // ======== run ========
+// sayHello();
+// sum(20, 9);
 
-const img = document.querySelector('img');
-const button = document.querySelector('button');
+// const img = document.querySelector('img');
+// const button = document.querySelector('button');
 
-button.addEventListener('click', () => {
-  img.src = dog;
-});
+// button.addEventListener('click', () => {
+//   img.src = dog;
+// });
